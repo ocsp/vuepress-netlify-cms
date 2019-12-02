4bd5896004b739c77f1b78d20e979005cd2fcc47
OCSP <58361304+ocsp@users.noreply.github.com>
2019-12-02 22:37:51 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,7 @@
 module.exports = {
   title: 'Kashima Press',
   description: 'Expression of ideas.',
-    head: [
-    ['link', { rel: 'icon', href: '//api.amanohina.com/favicon.ico' }]
-  ]
-}
-themeConfig: {
+  themeConfig: {
     docsDir: 'docs',
     repo: 'ocsp/vuepress-netlify-cms',
     sidebar: [
